af13d4dc756a054cb8dfa94255091462e5608e3d
Haruto Konno <qingdoujinye@gmail.com>
2021-10-01 02:33:53 +0900

    途中まで

--- lib/state_notifier/state.dart
+++ lib/state_notifier/state.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -5,31 +7,49 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'state.freezed.dart';
 
-void main() => runApp(const ProviderScope(child: App()));
+void main() => runApp(const App());
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'state_app',
-      home: MyHomePage(),
+    return MaterialApp(
+      home: StateNotifierProvider<_Controller, Counter>(
+        create: (context) => _Controller(),
+        child: HomePage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends ConsumerWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    return const Scaffold();
+  Widget build(BuildContext context) {
+    final controller = context.watch<_Controller>();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'increment'
+        ),
+      ),
+    );
   }
 }
 
+class _Controller extends StateNotifier<Counter> {
+  _Controller() : super(const Counter(count: 0));
+
+  void increment() => state = state.copyWith(
+        count: state.count + 1,
+      );
+}
+
+//model
 @freezed
 abstract class Counter with _$Counter {
+  //required最強
   const factory Counter({required int count}) = _Counter;
 }
