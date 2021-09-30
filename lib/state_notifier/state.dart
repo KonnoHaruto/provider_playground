8125f75fa68a2baaace3b2a1e6dc1c49ea2816c4
Haruto Konno <qingdoujinye@gmail.com>
2021-09-30 13:46:51 +0900

    state.dart

---
+++ lib/state_notifier/state.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'state.freezed.dart';
+
+void main() => runApp(const ProviderScope(child: App()));
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'state_app',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    return const Scaffold();
+  }
+}
+
+@freezed
+abstract class Counter with _$Counter {
+  const factory Counter({required int count}) = _Counter;
+}
