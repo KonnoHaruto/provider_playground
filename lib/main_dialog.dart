914cf76fe2c832637a3e543aebcda047b541e0cb
Haruto Konno <qingdoujinye@gmail.com>
2021-10-01 15:57:58 +0900

    commit

---
+++ lib/main_dialog.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //showDialog<T> Tはダイアログの表示結果の戻り値
+          showDialog<void>(
+              context: context,
+              builder: (context) {
+                final mediaQuery = MediaQuery.of(context);
+                return MediaQuery(
+                    data: mediaQuery.copyWith(
+                      viewInsets: mediaQuery.viewInsets +
+                          const EdgeInsets.symmetric(horizontal: 100),
+                    ),
+                    child: const AlertDialog(
+                      content: Text('Hello Dialog!'),
+                    ));
+              });
+        },
+      ),
+    );
+  }
+}
