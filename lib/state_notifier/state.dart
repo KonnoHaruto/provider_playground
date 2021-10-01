8602cd33867d1393edb309f2807598c503e2c19b
Haruto Konno <qingdoujinye@gmail.com>
2021-10-01 15:57:48 +0900

    fix

--- lib/state_notifier/state.dart
+++ lib/state_notifier/state.dart
@@ -10,7 +10,6 @@ void main() => runApp(const App());
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
