8f99eb1bfb13f9481ee632a2089cf38f15f2a586
Haruto Konno <qingdoujinye@gmail.com>
2021-10-01 14:56:54 +0900

    旧provider

--- lib/state_notifier/state.dart
+++ lib/state_notifier/state.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:state_notifier/state_notifier.dart';
 
 part 'state.freezed.dart';
