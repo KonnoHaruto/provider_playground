5789e8eddc79e71577acd5f416c59aee1540e71e
Haruto Konno <qingdoujinye@gmail.com>
2021-10-02 11:47:48 +0900

    雛形

---
+++ lib/main_show_text.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() => runApp(const ProviderScope(child: App()));
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Show_Text_App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: const Text('テキスト', style: TextStyle(fontSize: 30),),
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
